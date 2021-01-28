5099a86e76413e189d1cfa81f66cc222398ffce5
Carlos Trujillo <carlostsdv@gmail.com>
2021-01-28 21:36:43 +0100

    Trim file tree
    Delete leftover views engine boiler plate configuration

--- urlShortenerAPI/app.js
+++ urlShortenerAPI/app.js
@@ -15,9 +15,6 @@ connectDB()
 
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
