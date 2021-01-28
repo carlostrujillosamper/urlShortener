6d4165bcd45b93ff106c8e0b909ed8694c12ac0d
Carlos Trujillo <carlostsdv@gmail.com>
2021-01-28 17:50:26 +0100

    Add connection to db

---
+++ urlShortenerAPI/mongoDB/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const db = process.env.MONGOURI
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, {
+            useNewURLParser: true
+        });
+        console.log("Connected to DB");
+    }catch(err){
+        console.error(err.message);
+    }
+}
+
+module.exports = connectDB;
